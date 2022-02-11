52c917499857964378bbf5ec00e9dce89e26ffeb
Louis Dutton <clouisdutton@gmail.com>
2022-02-11 13:03:52 +0000

    setup basic AudioNode Chain

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,23 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import { Audio } from "./audio";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const ref = useRef<OscillatorNode>(null);
+
+  // useEffect(() => {
+  //   console.log(ref.current);
+  // });
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Audio>
+        <oscillator />
+        <gain />
+        {/* <destination /> */}
+      </Audio>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
